8142d2fdc1eaa2a0b770669c9cc4e881697736d8
Nik Ska <dokluch@gmail.com>
2022-08-01 11:09:23 +0400

    Updated Remove Disabled Cameras

--- src/contrib/remove_disabled_photos.py
+++ src/contrib/remove_disabled_photos.py
@@ -31,52 +31,50 @@ def remove_disabled_photos():
     counter_not_moved = 0
     counter_errors = 0
     counter_cameras = 0
-    lenght = len(chunk.cameras)
 
-    message = 'Starting to evaluate ' + str(lenght) + ' photos...'
-    print (message)
+    print (f"Starting to evaluate {len(chunk.cameras)} photos...")
 
     for camera in chunk.cameras:
         if camera.enabled is True:
-            counter_not_moved = counter_not_moved + 1
+            counter_not_moved += 1
             continue # skipping enabled cameras
 
         photo_path = Path(camera.photo.path)
-        photo_name = str(camera.label)
-        destination_dir = photo_path.parent / 'Disabled'
-        destination = destination_dir / photo_path.name
+        photo_name = camera.label
+        destination_dir = Path.joinpath(photo_path.parent, 'Disabled')
+        destination = Path.joinpath(destination_dir, photo_path.name)
 
         if not destination_dir.exists():
             try:
                 destination_dir.mkdir()
-                print ("Successfully created the directory %s " % destination_dir)
+                print (f"Successfully created the directory {destination_dir}")
             except OSError:
-                print ('Error creating %s' % destination_dir)
-                counter_errors = counter_errors + 1
+                print (f"Error creating {destination_dir}")
+                counter_errors += 1
                 continue # we can't create directory - thus we can't move photo - thus we shouldn't delete it
 
         try:
             if photo_path.is_file():
-                print ('Moving %s ...' % photo_name)
-                shutil.move(str(photo_path), str(destination))
+                print (f"Moving {photo_name} ...")
+                shutil.move(photo_path, destination)
 
-                counter = counter + 1
-                counter_cameras = counter_cameras + 1
+                counter += 1
+                counter_cameras += 1
             else:
-                print ('Photo %s does not exist!' % photo_name)
-                counter_cameras = counter_cameras + 1
-                counter_fail = counter_fail + 1
+                print (f"Photo {photo_name} does not exist!")
+                counter_cameras += 1
+                counter_fail += 1
 
             chunk.remove(camera)
 
         except OSError:
             counter_errors = counter_errors + 1
-            print ('Error %s!' % photo_name)
+            print (f"Error {photo_name}!")
 
-    message_end = 'Success, ' + str(counter) + ' photos moved, ' + str(counter_not_moved) + ' photos not moved.\nNumber of files unable to move: ' + str(counter_fail) + '\nNumber of cameras removed: ' + str(counter_cameras) + '\nNumber of unknown errorrs: '+ str(counter_errors)
+    message_end = f"Success, {counter}  photos moved, {counter_not_moved}  photos not moved.\nNumber of files unable to move: {counter_fail} \nNumber of cameras removed: {counter_cameras} \nNumber of unknown errorrs: {counter_errors}"
     print (message_end)
 
 
 label = "Scripts/Remove disabled photos"
 Metashape.app.addMenuItem(label, remove_disabled_photos)
-print("To execute this script press {}".format(label))
+print(f"To execute this script press {label}")
